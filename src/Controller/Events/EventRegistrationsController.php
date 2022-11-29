f2d49e5b73de3954ec4f1ef9ba8a8713f4f5bb5e
Papoel <pascal.briffard@gmail.com>
2022-11-29 23:56:04 +0100

    Add logic for verification registration

--- src/Controller/Events/EventRegistrationsController.php
+++ src/Controller/Events/EventRegistrationsController.php
@@ -2,34 +2,19 @@
 
 declare(strict_types=1);
 
-
 namespace App\Controller\Events;
 
 use App\Entity\Event\Event;
 use App\Entity\Event\Registration;
-use App\Form\EventFormType;
 use App\Form\RegistrationHelpFormType;
-use App\Repository\Creneau\CreneauRepository;
-use App\Repository\Event\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventRegistrationsController extends AbstractController
 {
-    #[Route(path: '/evenement/{slug}/inscription-aide', name: 'event_help_registration')]
-    public function index(Event $event, CreneauRepository $creneau): Response
-    {
-        return $this->render(view: 'inscriptions/index.html.twig', parameters: [
-            'event' => $event,
-            'registrations' => $event->getRegistrations(),
-            'creneaux' => $event->getCreneaux(),
-        ]);
-    }
-
     #[Route(path: '/evenement/{slug}/inscription-aide/creation', name: 'event_help_registration_create', methods: ['GET', 'POST'])]
     public function create(Event $event, Request $request, EntityManagerInterface $em): Response
     {
@@ -46,22 +31,25 @@ class EventRegistrationsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $registration->setEvent($event);
 
+            // 1. Je récupère l'email de l'aidant
             $email = $registration->getEmail();
-            $existingRegistration = $em->getRepository(entityName: Registration::class)->findOneBy(['email' => $email]);
+            // 2. Je récupère toutes les inscriptions de l'aidant à l'événement
+            $existingRegistration = $em->getRepository(entityName: Registration::class)->findBy(['email' => $email, 'event' => $event, 'activity' => $form->getData()->getActivity()]);
 
-            // WIP => Vérifier si email existe et si oui, vérifier event_id et creneau_id et activity sont <> de ceux de la nouvelle inscription
-            if ($existingRegistration && $existingRegistration->getEvent() === $event && $existingRegistration->getActivity() === $registration->getActivity()) {
-                $this->addFlash(type: 'danger', message: 'Cette adresse email est déjà enregistrée pour cet événement.');
-                return $this->redirectToRoute(route: 'event_help_registration', parameters: ['slug' => $event->getSlug()]);
+            // Vérifier les valeurs du tableau $registrationByActivity et les comparer avec les valeurs Activity de $existingRegistration
+            // Si les valeurs sont identiques, alors on ne peut pas créer l'inscription
+            if ($existingRegistration) {
+                $this->addFlash(type: 'danger', message: 'Vous êtes déjà inscrit à cette activité');
+
+                return $this->redirectToRoute(route: 'app_event_show', parameters: ['slug' => $event->getSlug()]);
             }
-
 
             // Récupérer dans un tableau les créneaux sélectionnés et faire un formatage pour les insérer dans la table registration
             $creneau_choices = $form->get('creneauChoices')->getData();
             $creneau_choices = array_map(
                 static function ($creneau) {
-                    return $creneau->getStartsAt()->format('H:i') . ' - ' . $creneau->getEndsAt()->format('H:i');
-                    },
+                    return $creneau->getStartsAt()->format('H:i').' - '.$creneau->getEndsAt()->format('H:i');
+                },
                 $creneau_choices
             );
 
@@ -70,13 +58,12 @@ class EventRegistrationsController extends AbstractController
             $em->persist($registration);
             $em->flush();
 
-            $this->addFlash(type: 'success', message: 'Merci, votre inscription à l\'événement .$event->getName(). à bien été prise en compte.');
+            $this->addFlash(type: 'success', message: 'Merci, votre inscription à l\'événement : '.$event->getName().' à bien été prise en compte.');
 
-            return $this->redirectToRoute(route: 'event_help_registration', parameters: ['slug' => $event->getSlug()]);
-
+            return $this->redirectToRoute(route: 'app_event_show', parameters: ['slug' => $event->getSlug()]);
         }
 
-        return $this->render('inscriptions/create.html.twig', [
+        return $this->render(view: 'inscriptions/create.html.twig', parameters: [
             'event' => $event,
             'form' => $form->createView(),
         ]);
