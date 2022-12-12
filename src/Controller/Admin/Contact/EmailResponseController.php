5fc8697744d31af91aed70c1f23050f161ea815b
Papoel <pascal.briffard@gmail.com>
2022-12-12 18:01:21 +0100

    Code Clean

--- src/Controller/Admin/Contact/EmailResponseController.php
+++ src/Controller/Admin/Contact/EmailResponseController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin\Contact;
 
-use App\Entity\Contact\Contact;
 use App\Form\ResponseEmailFormType;
 use App\Repository\ContactRepository;
 use App\Services\MailService;
