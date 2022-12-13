baddec1b770d234a1e749c630ab2b6e871c9021d
Papoel <pascal.briffard@gmail.com>
2022-12-13 20:00:05 +0100

    refactorisation path ContactRepository

--- src/Entity/Contact/Contact.php
+++ src/Entity/Contact/Contact.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity\Contact;
 
-use App\Repository\ContactRepository;
+use App\Repository\Contact\ContactRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
