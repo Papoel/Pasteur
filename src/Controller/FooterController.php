46f751bab23cd7fd3f2c86cd39805c3f9b0a87b0
Papoel <pascal.briffard@gmail.com>
2022-12-15 00:32:45 +0100

    Add component dynamic for team ape

---
+++ src/Controller/FooterController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\User\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class FooterController extends AbstractController
+{
+    public function apeTeam(UserRepository $user): Response
+    {
+        $president = $user->findByRole(role: "PRESIDENT");
+        $tresorier = $user->findByRole(role: "TRESORIER");
+        $secretaire = $user->findByRole(role: "SECRETAIRE");
+        $administrateur = $user->findByRole(role: "ADMIN");
+
+        return $this->render(view: 'components/_card-member.html.twig', parameters: [
+            'president' => $president,
+            'tresorier' => $tresorier,
+            'secretaire' => $secretaire,
+            'administrateur' => $administrateur
+        ]);
+    }
+}
