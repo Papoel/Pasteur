5712d143b411a8b49ae0c890f88cf47b41adf5c3
Papoel <pascal.briffard@gmail.com>
2022-12-21 15:45:23 +0100

    WIP | add registration gestion for Event

---
+++ src/Controller/Events/EventRegistrationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Events;
+
+use App\Entity\Event\Event;
+use App\Entity\Event\RegistrationEvent;
+use App\Form\EventRegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventRegistrationController extends AbstractController
+{
+    // TODO : Renvoyer la liste des inscrits pour les ADMINS
+    #[Route('/evenement/{slug}/inscription-evenement', name: 'app_event_registrations_index' , methods: ['GET'])]
+    public function index(Event $event): Response
+    {
+        $registrations = $event->getRegistrationEvents()->toArray();
+
+        return $this->render(view: 'events/registrations/list.html.twig' , parameters: [
+            'event' => $event,
+            'registrations' => $registrations
+        ]);
+    }
+
+
+    #[Route('/evenement/{slug}/inscription-evenement/create', name: 'app_event_registrations_create' , methods: ['GET', 'POST'])]
+    public function create(Event $event, Request $request, EntityManagerInterface $em): Response
+    {
+        $registration = new RegistrationEvent();
+
+        $form = $this->createForm(type: EventRegistrationFormType::class, data: $registration);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $registration->setEvent($event);
+
+            $em->persist($registration);
+            $em->flush();
+
+            $this->addFlash(type: 'success', message: "Merci, vous êtes inscrit !");
+
+            return $this->redirectToRoute(route: 'app_event_registrations_index', parameters: ['event' => $event->getId()]);
+        }
+
+        return $this->renderForm(view: 'events/registrations/create.html.twig', parameters: [
+            'event' => $event,
+            'form' => $form
+        ]);
+    }
+
+}
---
+++ src/Form/EventRegistrationFormType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event\RegistrationEvent;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add(child: 'firstname', type: TextType::class, options: [
+                'label' => 'Prénom :',
+                'label_attr' => [
+                    'class' => 'block text-gray-500 uppercase tracking-wider text-sm font-bold',
+                ],
+                'attr' => [
+                    'placeholder' => 'Bruce Wayne',
+                    'class' =>
+                        'w-full bg-white rounded focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base 
+                        outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+                    '
+                ],
+                'required' => false,
+            ])
+
+            ->add(child: 'lastname', type: TextType::class, options: [
+                'label' => 'Nom :',
+                'label_attr' => [
+                    'class' => 'block text-gray-500 uppercase tracking-wider text-sm font-bold',
+                ],
+                'attr' => [
+                    'placeholder' => 'Bruce Wayne',
+                    'class' =>
+                        'w-full bg-white rounded focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base 
+                        outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+                    '
+                ],
+                'required' => false,
+            ])
+
+            ->add(child: 'telephone', type: TextType::class, options: [
+                'label' => 'Téléphone :',
+                'label_attr' => [
+                    'class' => 'block text-gray-500 uppercase tracking-wider text-sm font-bold',
+                ],
+                'attr' => [
+                    'placeholder' => '06 66 66 66 66',
+                    'type' => 'tel',
+                    'class' =>
+                        'w-full bg-white rounded focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base 
+                        outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
+                    ,
+                ],
+                'required' => false,
+            ])
+
+            ->add(child: 'email', type: EmailType::class, options: [
+                'label' => 'Email :',
+                'label_attr' => [
+                    'class' => 'block text-gray-500 uppercase tracking-wider text-sm font-bold',
+                ],
+                'attr' => [
+                    'placeholder' => 'batman@goham.city',
+                    'class' =>
+                        'w-full bg-white rounded focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base 
+                        outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+                    '
+                ],
+                'required' => false,
+            ])
+
+            ->add(child: 'children', type: CollectionType::class,options: [
+                'allow_add' => true,
+                'allow_delete' => true,
+                'entry_type' => AddChildrenFormType::class,
+                'by_reference' => true
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RegistrationEvent::class,
+        ]);
+    }
+}
