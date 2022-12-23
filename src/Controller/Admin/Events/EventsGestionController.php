ca9a72889a12df6726e1498cb3c10b88e431cd55
Papoel <pascal.briffard@gmail.com>
2022-12-24 00:35:28 +0100

    WIP | List Registrations for Admin

--- src/Controller/Admin/Events/EventsGestionController.php
+++ src/Controller/Admin/Events/EventsGestionController.php
@@ -5,20 +5,23 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Events;
 
 use App\Entity\Event\Event;
+use App\Repository\Event\EventRepository;
+use App\Repository\Event\RegistrationEventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventsGestionController extends AbstractController
 {
-// TODO : A retravailler -> Renvoie la liste des inscrits pour les ADMINS
-    #[Route('/evenement/{slug}/inscription-evenement', name: 'app_event_registrations_index', methods: ['GET'])]
-    public function index(Event $event): Response
+    // TODO : A retravailler -> Renvoie la liste des inscrits pour les ADMINS
+    #[Route('/admin/details/inscription', name: 'app_event_registrations_index', methods: ['GET'])]
+    public function index(EventRepository $eventRepository, RegistrationEventRepository $registrations): Response
     {
-        $registrations = $event->getRegistrationEvents()->toArray();
+        $events = $eventRepository->findAll();
+        $registrations = $registrations->findAll();
 
         return $this->render(view: 'events/registrations/list.html.twig', parameters: [
-            'event' => $event,
+            'events' => $events,
             'registrations' => $registrations,
         ]);
     }
