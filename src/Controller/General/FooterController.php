9b5bab91240ace73e1b357398a7f55d5c44956f7
Papoel <pascal.briffard@gmail.com>
2022-12-27 11:12:05 +0100

    add folder General for Architecture

--- src/Controller/General/FooterController.php
+++ src/Controller/General/FooterController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Controller\General;
 
 use App\Repository\User\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
