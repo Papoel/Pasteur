9ecde1f0376fbdbf8fe698e1fa5de156d446a950
Papoel <pascal.briffard@gmail.com>
2023-01-07 13:59:58 +0100

    Add Webhook and data processing

--- src/Controller/Payment/CheckoutSessionController.php
+++ src/Controller/Payment/CheckoutSessionController.php
@@ -6,136 +6,130 @@ namespace App\Controller\Payment;
 
 use App\Entity\Event\Event;
 use App\Entity\Event\RegistrationEvent;
-use App\Entity\Payment;
-use App\Services\DataSaveSessionServices;
 use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Checkout\Session;
-use Stripe\Customer;
-use Stripe\Exception\ApiErrorException;
 use Stripe\Stripe;
-use Stripe\StripeClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CheckoutSessionController extends AbstractController
 {
-    /**
-     * @param DataSaveSessionServices $dataSaveSessionServices
-     * @param RequestStack            $requestStack
-     * @param EntityManagerInterface  $entityManager
-     */
-    public function __construct(
-        private DataSaveSessionServices $dataSaveSessionServices,
-        private RequestStack $requestStack,
-        private EntityManagerInterface $entityManager
-    ) {
+    public function __construct(private EntityManagerInterface $em)
+    {
     }
 
-    /**
-     * @throws ApiErrorException
-     */
     #[Route('/stripe/create-session', name: 'app_create_checkout_session', methods: ['POST' , 'GET'])]
-    public function index(): Response
+    public function checkout(Request $request): Response
     {
         $STRIPE_KEY_SECRET = $this->getParameter(name: 'STRIPE_KEY_SECRET');
         Stripe::setApiKey($STRIPE_KEY_SECRET);
-        $stripe = new StripeClient($STRIPE_KEY_SECRET);
+        // $stripe = new StripeClient($STRIPE_KEY_SECRET);
 
-        $session = $this->dataSaveSessionServices;
-        // 2. On crée une session de paiement
-        $DOMAIN = 'https://127.0.0.1:8000/';
-
+        $session = $request->getSession()->get(name: 'details_inscription');
         /** On récupère les données de la session pour les détails de l'acheteur */
         $customer_details = [
-            'email' => $session->getData(key: 'representant_legal_email') ,
-            'name' => $session->getData(key: 'representant_legal') ,
-            'phone' => $session->getData(key: 'representant_legal_telephone') ,
+            'email' => $session['representant_legal_email'],
+            'name' => $session['representant_legal'],
+            'phone' => $session['representant_legal_telephone'],
         ];
 
         /** Créez un nouveau client Stripe en utilisant les détails du client */
-        $customer = Customer::create(params: [
+        /*$customer = Customer::create(params: [
             'email' => $customer_details['email'] ,
             'name' => $customer_details['name'] ,
             'phone' => $customer_details['phone'] ,
-        ]);
+        ]);*/
+
+       //dd($session);
 
         $checkout_session = Session::create(params: [
-            'customer_email' => $session->getData(key: 'representant_legal_email') ,
+            'customer_email' => $session['representant_legal_email'],
             'mode' => 'payment' ,
-            'success_url' => $DOMAIN . 'inscription/success/{CHECKOUT_SESSION_ID}' ,
-            'cancel_url' => $DOMAIN . 'inscription/cancel/{CHECKOUT_SESSION_ID}' ,
+            'success_url' => $this->generateUrl(
+                route: 'app_payment_stripe_success_payment',
+                parameters: [],
+                referenceType: UrlGeneratorInterface::ABSOLUTE_URL
+            ) ,
+            'cancel_url' => $this->generateUrl(
+                route: 'app_payment_stripe_cancel_payment',
+                parameters: [],
+                referenceType: UrlGeneratorInterface::ABSOLUTE_URL
+            ) ,
             'payment_method_types' => ['card'] ,
             'metadata' => [
-                'payment_id' => $session->getData(key: 'created_at')->format('Ymd')
-                    . '-' .
-                    $session->getData(key: 'inscription_event_name')
-                    . '-' .
-                    uniqid(prefix: 'paiement-', more_entropy: false) ,
-                'evenement' => $session->getData(key: 'inscription_event_name') ,
-                'inscription_id' => $session->getData(key: 'registration_id') ,
-                'places_reservees' => $session->getData(key: 'reserved_places') ,
-                'prix_unitaire' => $session->getData(key: 'unit_price') ,
-                'prix_total' => $session->getData(key: 'total_price') ,
-                'parent_nom' => $session->getData(key: 'representant_legal') ,
-                'parent_email' => $session->getData(key: 'representant_legal_email') ,
-                'parent_telephone' => $session->getData(key: 'representant_legal_telephone') ,
-                'date_de_creation' => $session->getData(key: 'created_at')->format('d-m-Y') ,
-                'evenement_debut' => $session->getData(key: 'event_start')->format('d-m-Y' . ' à ' . 'H:i') ,
+                // Create payment intent with a unique ID and date now
+                'stripe_session_id' => $session['stripe_session_id'] ,
+                'payment_id' => $session['id'] . '_' . $session['date_creation_session'],
+                'evenement' => $session['evenement_nom'],
+                'evenement_slug' => $session['evenement_slug'],
+                'inscription_id' => $session['inscription_id'],
+                'places_reservees' => $session['places_reservees'],
+                'prix_unitaire' => $session['evenement_prix'],
+                'prix_total' => $session['total'] ,
+                'parent_nom' => $session['representant_legal'],
+                'parent_email' => $session['representant_legal_email'],
+                'parent_telephone' => $session['representant_legal_telephone'],
+                'date_de_creation' => $session['date_creation_session'],
             ] ,
             'line_items' => [[
-                'quantity' => $session->getData(key: 'reserved_places') ,
+                'quantity' => $session['places_reservees'] ,
                 'price_data' => [
                     'currency' => 'EUR' ,
-                    'unit_amount' => $session->getData(key: 'unit_price') ,
+                    'unit_amount' => $session['evenement_prix'],
                     'product_data' => [
-                        'name' => $session->getData(key: 'inscription_event_name') ,
+                        'name' => $session['evenement_nom'],
                     ] ,
                 ] ,
             ]] ,
         ]);
+        $checkout_sessionId = $checkout_session->id;
+        // Set in details_inscription session the checkout_sessionId
+        $session['checkout_sessionId'] = $checkout_sessionId;
 
-        $checkout_session_id = $checkout_session->id;
-
+        // $checkout_session_id = $checkout_session->id;
 
         /** Récupérer l'événement auquel l'utilisateur s'inscrit */
-        /*$registrationEventName = $session->getData(key: 'inscription_event_name');
-        $event = $this->entityManager
-            ->getRepository(Event::class)
-            ->findOneBy(['slug' => $registrationEventName]);*/
+        /*$registrationEventName = $session['evenement_slug'];
+        $event = $this->em
+            ->getRepository(entityName: Event::class)
+            ->findOneBy(['slug' => $registrationEventName]);
 
         /** Récupérer l'inscription de l'utilisateur */
-        /*$registrationId = $session->getData(key: 'registration_id');
-        $registration = $this->entityManager
-            ->getRepository(RegistrationEvent::class)
+        /**$registrationId = $session['inscription_id'];
+        $registration = $this->em
+            ->getRepository(entityName: RegistrationEvent::class)
             ->findOneBy(['id' => $registrationId]);*/
 
-        /** enregistrer dans la table "paiement" si le paiement est réussis */
-        /*if ($checkout_session->payment_intent === 'succeeded') {
+        /** Enregistrer dans la table "paiement" si le paiement est réussis */
+        /*if ($checkout_session->status === 'complete') {
             $payment = new Payment();
-            $payment->setEvent($event);
-            $payment->setRegistrationEvent($registration);
-            $payment->setStripeSessionId($checkout_session_id);
-            $payment->setStripePaymentIntentId($checkout_session->payment_intent);
-            $payment->setStripePaymentIntentStatus($checkout_session->payment_status);
+            $payment->setEvent(event: $event);
+            $payment->setRegistrationEvent(registrationEvent: $registration);
+            $payment->setStripeSessionId(stripeSessionId: $checkout_session->id);
+            $payment->setStripePaymentIntentId(stripePaymentIntentId: $checkout_session->payment_intent);
+            $payment->setStripePaymentIntentStatus(stripePaymentIntentStatus: $checkout_session->payment_status);
 
-            $this->entityManager->persist($payment);
-            $this->entityManager->flush();
+            $this->em->persist(entity: $payment);
+            $this->em->flush();
+        } else {
+            $this->addFlash(type: 'danger', message: 'quelque chose ne va pas');
         }*/
 
-        return $this->redirect($checkout_session->url);
+        return $this->redirect($checkout_session->url, status: 303);
     }
 
     /** Redirection vers la page de succès */
-    #[Route('/inscription/success/{CHECKOUT_SESSION_ID}', name: 'app_payment_stripe_success_payment', methods: ['GET'])]
+    #[Route('/inscription/success', name: 'app_payment_stripe_success_payment', methods: ['GET'])]
     public function successPayment(): Response
     {
         return $this->render(view: 'stripe/success.html.twig');
     }
 
     /** Redirection vers la page d'annulation */
-    #[Route('/inscription/cancel/{CHECKOUT_SESSION_ID}', name: 'app_payment_stripe_cancel_payment', methods: ['GET'])]
+    #[Route('/inscription/cancel', name: 'app_payment_stripe_cancel_payment', methods: ['GET'])]
     public function cancelPayment(): Response
     {
         return $this->render(view: 'stripe/cancel.html.twig');
--- src/Controller/Payment/WebhooksStripeController.php
+++ src/Controller/Payment/WebhooksStripeController.php
@@ -5,89 +5,81 @@ namespace App\Controller\Payment;
 use App\Entity\Event\Event;
 use App\Entity\Event\RegistrationEvent;
 use App\Entity\Payment;
-use App\Services\DataSaveSessionServices;
 use Doctrine\ORM\EntityManagerInterface;
 use Stripe\Exception\SignatureVerificationException;
-use Stripe\Stripe;
 use Stripe\Webhook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use UnexpectedValueException;
 
 class WebhooksStripeController extends AbstractController
 {
-    public function __construct(
-        private DataSaveSessionServices $dataSaveSessionServices,
-        private RequestStack $requestStack,
-        private EntityManagerInterface $entityManager
-    ) {
-    }
-
     #[Route('/webhooks', name: 'app_webhooks_stripe')]
-    public function index()
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $STRIPE_KEY_SECRET = $this->getParameter(name: 'STRIPE_KEY_SECRET');
-        Stripe::setApiKey($STRIPE_KEY_SECRET);
-        $endpoint_secret = 'whsec_bcc6ea4621e7b8f04a9bb5c218e05280ccdc950616587709936411f74a926fa1';
-        /**
-         * Les webhooks de Stripe sont des requêtes POST avec un corps JSON. Le JSON brut peut généralement être lu à
-         * partir de stdin, mais cela peut varier en fonction de la configuration du serveur.
-         *
-         * Les données du webhook ne seront pas disponibles dans la superglobale $_POST car les demandes de webhook de
-         * Stripe ne sont pas envoyées au format codé par formulaire.
-         */
+        $session = $request->getSession()->get(name: 'details_inscription');
+
+        $endpoint_secret = $this->getParameter(name: 'STRIPE_WEBHOOK_SECRET');
 
         $payload = @file_get_contents(filename: 'php://input');
-        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
 
-        /** Instancier un Paiement pour Sauvegarder en base de données **/
-        $payment = new Payment();
+        //$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $header = 'Stripe-Signature';
+        $signature = $request->headers->get($header);
 
+        $sig_header = $signature;
         $event = null;
+
+        // TODO: Besoin ?
+        //$STRIPE_KEY_SECRET = $this->getParameter(name: 'STRIPE_KEY_SECRET');
+        //Stripe::setApiKey($STRIPE_KEY_SECRET);
+
 
         try {
             $event = Webhook::constructEvent($payload, $sig_header, $endpoint_secret);
         } catch (UnexpectedValueException $e) {
             // Payload invalide
+            echo $e->getMessage();
             http_response_code(response_code: 400);
             exit();
         } catch (SignatureVerificationException $e) {
             // Signature Invalide
+            echo $e->getMessage();
             http_response_code(response_code: 400);
             exit();
         }
 
         if ($event->type === 'checkout.session.completed') {
-            /** Récupérer l'événement auquel l'utilisateur s'inscrit */
-            $session = $this->dataSaveSessionServices;
-            $registrationEventName = $session->getData(key: 'inscription_event_name');
+            $metadata = $event->data->object->metadata;
+            // Création de fichier pour le debug uniquement
+            // file_put_contents(filename: 'details_inscription', data: $metadata);
+            // file_put_contents(filename: 'checkout.completed', data: $event);
+            $registrationEventSlug = $metadata['evenement_slug'];
+            $eventRegistered = $em
+            ->getRepository(entityName: Event::class)
+            ->findOneBy(['slug' => $registrationEventSlug]);
 
-            $event = $this->entityManager
-                ->getRepository(Event::class)
-                ->findOneBy(['slug' => $registrationEventName]);
-
-            /** Récupérer l'inscription de l'utilisateur */
-            $registrationId = $session->getData(key: 'registration_id');
-            $registration = $this->entityManager
-                ->getRepository(RegistrationEvent::class)
-                ->findOneBy(['id' => $registrationId]);
+            $registrationId = $metadata['inscription_id'];
+            $registration = $em
+            ->getRepository(entityName: RegistrationEvent::class)
+            ->findOneBy(['id' => $registrationId]);
 
             // Passer à true le champ Paid de la table RegistrationEvent
             $registration->setPaid(Paid: true);
 
             // Sauvegarder les informations de paiement en base de données
             $payment = new Payment();
-            $payment->setEvent(event: $event);
+            $payment->setEvent(event: $eventRegistered);
             $payment->setRegistrationEvent(registrationEvent: $registration);
-            $payment->setStripeSessionId(stripeSessionId: $event->getData(key: 'checkout_session_id'));
-            $payment->setStripePaymentIntentId(stripePaymentIntentId: $event->getData(key: 'payment_intent'));
-            $payment->setStripePaymentIntentStatus(stripePaymentIntentStatus: $event->getData(key: 'payment_status'));
+            $payment->setStripeSessionId(stripeSessionId: $event->id);
+            $payment->setStripePaymentIntentId(stripePaymentIntentId: $event->data->object->payment_intent);
+            $payment->setStripePaymentIntentStatus(stripePaymentIntentStatus: $event->data->object->payment_status);
 
-            $this->entityManager->persist($payment);
-            $this->entityManager->flush();
+            $em->persist($payment);
+            $em->flush();
         }
-
-        http_response_code(response_code: 200);
+        return new Response(content: Response::HTTP_OK);
     }
 }
