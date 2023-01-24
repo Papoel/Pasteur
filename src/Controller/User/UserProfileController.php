94a8fbbb9b576edb38c8209050901d755da84356
Papoel <pascal.briffard@gmail.com>
2023-01-24 20:43:27 +0100

    add feature for ask delete account

--- src/Controller/User/UserProfileController.php
+++ src/Controller/User/UserProfileController.php
@@ -4,13 +4,23 @@ namespace App\Controller\User;
 
 use App\Entity\User\User;
 use App\Repository\Event\RegistrationEventRepository;
+use App\Services\MailService;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserProfileController extends AbstractController
 {
+    public function __construct(
+        private MailerInterface $mailer,
+    ) {
+    }
     #[Route('/user/profile', name: 'app_user_profile')]
+    #[isGranted('ROLE_ADMIN')]
     public function index(RegistrationEventRepository $registrationEventRepository): Response
     {
         /** @var User $user */
@@ -23,5 +33,35 @@ class UserProfileController extends AbstractController
             'user' => $user,
             'registrations' => $registrations,
         ]);
+    }
+
+    #[Route('/user/profile/delete', name: 'app_user_profile_delete')]
+    #[isGranted('ROLE_ADMIN')]
+    public function DeleteAccount(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $email = (new TemplatedEmail())
+            ->from($user->getEmail())
+            ->to(new Address(address: 'aperousiespasteur@gmail.com'))
+            ->subject(subject: 'Demande de suppression de compte!')
+
+            // path of the Twig template to render
+            ->htmlTemplate(template: 'emails/delete_account.html.twig')
+
+            // pass variables (name => value) to the template
+            ->context([
+                'username' => $user->getFirstname() . ' ' . $user->getLastname(),
+            ]);
+
+            $this->mailer->send($email);
+
+            $this->addFlash(
+                type: 'success',
+                message: 'Votre demande de suppression de compte a bien été transmise à l\'administrateur du site.'
+            );
+
+        return $this->redirectToRoute(route: 'app_user_profile');
     }
 }
