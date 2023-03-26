ad908583c5d6a8dd417e33f0af7a80a4cf573be8
Papoel <pascal.briffard@gmail.com>
2023-03-26 18:53:14 +0200

    Add comments for identify the utility of file

--- Sendmail.php
+++ Sendmail.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+/**
+ * Ce fichier est créé dans le but de pouvoir envoyer des emails en asynchrone
+ * Ce fichier est appelé par une Cron Task au sein de mon hébergeur
+ */
+
 // Chemin vers le fichier console de Symfony
 $consolePath = __DIR__ . '/bin/console';
 
